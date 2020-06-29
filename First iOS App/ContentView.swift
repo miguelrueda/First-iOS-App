ae87387e99ba9c720c5a47664b196263d8641a70
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-06-29 11:06:37 -0500

    Initial Commit

---
+++ First iOS App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First iOS App
+//
+//  Created by Miguel Rueda on 29/06/20.
+//  Copyright © 2020 Miguel Rueda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
